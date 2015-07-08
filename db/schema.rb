1a080116f1e4467c064695d8ac8c799bc8f4098a
Nguyen Cuong <cuongnx2603@gmail.com>
2015-07-08 13:45:19 +0700

    Use devise for user signin signout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708053246) do
+ActiveRecord::Schema.define(version: 20150708064143) do
 
   create_table "addresses", force: :cascade do |t|
     t.float    "lon"
@@ -66,8 +66,6 @@ ActiveRecord::Schema.define(version: 20150708053246) do
   add_index "rooms", ["address_id"], name: "index_rooms_on_address_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
     t.string   "name"
     t.integer  "age"
     t.string   "job"
@@ -76,8 +74,21 @@ ActiveRecord::Schema.define(version: 20150708053246) do
     t.string   "provider"
     t.string   "uid"
     t.integer  "role"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
