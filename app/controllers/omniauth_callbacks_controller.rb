ce53a1ee6241d11b12554988d5ac3e5b3454a16b
cam <daothanhcam@gmail.com>
2015-07-09 13:55:16 +0700

    Authenticate via google

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,43 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    generic_callback Settings.provider.google
+  end
+
+  def generic_callback provider
+    @identity = Identity.find_for_oauth env["omniauth.auth"]
+    @user = @identity.user || current_user
+    if @user.nil?
+      @user = User.create email: @identity.email || ""
+      @identity.update_attribute :user_id, @user.id
+    end
+
+    if @user.email.blank? && @identity.email
+      @user.update_attribute :email, @identity.email
+    end
+
+    if @user.persisted?
+      @identity.update_attribute :user_id, @user.id
+      @user = FormUser.find @user.id
+      sign_in_and_redirect @user, event: :authentication
+      if is_navigational_format?
+        set_flash_message :notice, :success, kind: provider.capitalize
+      end
+    else
+      session["devise.#{provider}_data"] = env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def upgrade
+    if params[:provider] == Settings.provider.google
+      scope = "email, profile, offline, https://www.googleapis.com/auth/admin.directory.user"
+    end
+    redirect_to user_omniauth_authorize_path params[:provider],
+      flash: {scope: scope}
+  end
+
+  def setup
+    request.env["omniauth.strategy"].options["scope"] ||= flash[:scope]
+    render text: t("Setup complete."), status: 404
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root "static_pages#home"
 
-  devise_for :users, path_names: {sign_in: "login", sign_up: "register"}
+  devise_for :users, class_name: "FormUser",
+    controllers: {omniauth_callbacks: "omniauth_callbacks",
+                  registrations: "registrations"}
+
+  devise_scope :user do
+    get "/users/auth/:provider/upgrade" => "omniauth_callbacks#upgrade",
+      as: :user_omniauth_upgrade
+    get "/users/auth/:provider/setup", to: "omniauth_callbacks#setup"
+  end
 end
