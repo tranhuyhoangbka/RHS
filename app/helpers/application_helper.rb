e2da4e376143c139a70897f8beb6950c759bc514
HoangVanTrinh92 <hoang.van.trinh@framgia.com>
2015-07-15 18:19:39 +0700

    design-add-address-interface

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
   def link_to_add_fields name, f, association
     new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
+    fields = f.fields_for(association, new_object,
+      child_index: "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", f: builder)
     end
-    link_to name, "#",
-      onclick: h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"), remote: true
+    link_to "#",
+      onclick: ("add_fields(this, \"#{association}\",
+      \"#{escape_javascript(fields)}\")"),
+      class: "btn btn-info add_nested_fields", remote: true do
+        raw "<i class=\"glyphicon glyphicon-plus\"></i> " + name
+    end
   end
 end
