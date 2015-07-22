76da98ac66f1a6e5c4d4005c4da122324215090d
cam <daothanhcam@gmail.com>
2015-07-22 16:24:06 +0700

    create add blocks

--- app/models/address.rb
+++ app/models/address.rb
@@ -11,5 +11,11 @@ class Address < ActiveRecord::Base
 
   accepts_nested_attributes_for :images, allow_destroy: true
 
+  delegate :province, to: :region
+
+  scope :by_province, -> province{joins(:region)
+    .where("regions.province = ?", province)
+    .order(created_at: :desc).limit(Settings.num_of_adds_in_regions)}
+
   enum type: [:apartment, :villa]
 end
