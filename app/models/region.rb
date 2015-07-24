b754119020082f9b7b434fdb28d8ab28c06bb2c7
cam <daothanhcam@gmail.com>
2015-07-24 16:38:08 +0700

    show-address-by-regions

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,7 @@ class Region < ActiveRecord::Base
   include RailsAdmin::Region
 
   has_many :addresses, dependent: :destroy
+
+  scope :get_special, -> {where(special_location: true)
+    .limit(Settings.num_of_show_location)}
 end
