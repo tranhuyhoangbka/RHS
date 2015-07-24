257e75cfe90806fa63b629d7a549e882eff74423
hoang <hoangth92.nd@gmail.com>
2015-07-24 15:02:22 +0700

    fix address show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :store_location
-
-  def store_location
-    session[:return] = params[:return] if params[:return].present?
-  end
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) <<
       [:name, :age, :address, :job, :phone_number]
-  end
-
-  def after_sign_in_path_for resource
-    session.delete :return || root_path
   end
 end
